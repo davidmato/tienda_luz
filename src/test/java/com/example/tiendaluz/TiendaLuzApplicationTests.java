8ad3b0345f1a03837f27f85f55076914c7dd07cc
davidmato <dmatodelalozapalomo@safareyes.es>
2024-09-25 20:08:54 +0200

    commit de entrega

--- src/test/java/com/example/tiendaluz/TiendaLuzApplicationTests.java
+++ src/test/java/com/example/tiendaluz/TiendaLuzApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.tiendaluz;
 
+import com.example.tiendaluz.modelos.*;
+import com.example.tiendaluz.repositorios.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class TiendaLuzApplicationTests {
+    @Autowired
+    private ClienteRepositorio clienteRepositorio;
+
 
     @Test
     void contextLoads() {
+
+        List<Cliente> clientes = clienteRepositorio.findAll();
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente);
+        }
+
     }
 
 }
